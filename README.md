7ac79caa042eb333128967f7245610856ebd7b35
root <diwakarsharma603@gmail.com>
2020-09-20 22:49:27 +0530

    Migration, Seeds & Read Me file updated

---
+++ README.md
@@ -0,0 +1,11 @@
+Please follow the below steps.
+
+1 --> Clone the repo on your local machine.
+2 --> Run the command given below.
+
+    sudo php artisan migrate
+    sudo php artisan db:seed
+
+3 --> Login via name (username).
+
+Note : Please don't try to change the vendor folder because its having the methods for authentication .
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon as Carbon;
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        for ($i = 1; $i < 5; $i++) {
+            User::create(
+                [
+                    'id'                => $i*10,
+                    'name'              => 'test'.$i,
+                    'email'             => 'test'.$i.'@gmail.com',
+                    'profile_img'       => 'default.jpg',
+                    'password'          => bcrypt('12345678'),
+                    'remember_token'    => NULL,
+                    'created_at'        => Carbon::now(),
+                    'updated_at'        => Carbon::now(),
+                ],
+            );
+        }
     }
 }
